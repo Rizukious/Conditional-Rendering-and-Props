799480617b4c3d85db0099a36ab23e7fc5820fab
rizukiwahyudi <rizukiwahyudi@gmail.com>
2021-01-27 13:55:05 +0700

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row header mb-5 p-3">
+        <div class="col-lg-12">
+          <h2>
+            What I do Today?
+          </h2>
+        </div>
+      </div>
+
+      <div class="row content">
+        <div class="col-lg-12">
+          <div class="pekerjaan-rumah">
+            <!-- v-for digunakan sebagai perulangan data JSON -->
+            <!-- v-bind:key digunakan sebagai pengambil key didalam JSON -->
+            <div class="row">
+              <div v-for="tugasSaya in tugas" v-bind:key="tugasSaya.id" class="mb-4 tugas col-lg-12 text-left">
+                <!-- melempar data dengan props -->
+                <itemTugas v-bind:tugasSaya="tugasSaya" />
+            </div>  
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import "bootstrap/dist/css/bootstrap.css";
+import itemTugas from "./components/itemTugas.vue";
 
 export default {
   name: 'App',
+  data : function() {
+    return {
+      tugas : [
+      {
+        // id sebagai Properties dan sebagai key
+        // 1 sebagai values/nilai
+        id : 1,
+        namaTugas : "Membersihkan Toilet",
+        isDone : false
+      },
+      {
+        id:2,
+        namaTugas : "Menyapu Halaman",
+        isDone : false
+      },
+      {
+        id:3,
+        namaTugas : "Bermain Warnet",
+        isDone : false
+      }
+    ]
+    }
+  },
   components: {
-    HelloWorld
+    itemTugas,
   }
 }
 </script>
@@ -24,5 +71,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.header {
+  background: #1abc9c;
+}
+.header h2 {
+  margin: 0;
+}
+.tugas .item {
+  font-size: 20px;
+}
+.item-completed {
+  color: #1abc9c;
 }
 </style>
---
+++ src/components/itemTugas.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="form-check">
+            <input class="form-check-input" onclick="" type="checkbox" value="" id="defaultCheck1">
+                  <!-- Conditional Rendering Vue-->
+                  <!-- and Props -->
+            <label class="form-check-label item" v-bind:class="{'item-completed' : tugasSaya.isDone}" for="defaultCheck1">
+                    {{tugasSaya.namaTugas}}
+            </label>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    nama : "ItemTugas",
+    props : ["tugasSaya"]
+};
+</script>
+
+<style scoped>
+
+</style>
